25f8f8b9c6ef274a51a4b8e15b92d3607feeacdf
Jake Persons <jake.persons@outlook.com>
2022-08-16 17:29:51 -0500

    Updating README file

--- translate.py
+++ translate.py
@@ -6,39 +6,23 @@ import argparse
 from config import API_KEY
 
 
-"""
-In order to use the Google Translate API, you need an API KEY.
-Create a Google account, and navigate to console.cloud.google.com.
-In the header there is a dropdown, click it.
-A screen should pop up, and there should be a button to create a new project.
-Create a new project.
-Click the hamburger button in the top left, hover over APIs and Services.
-Click Library, and search for the translate API.
-Enable the Cloud Translation API, you will need to enter billing info at this point.
-The first 500k characters every month are free as of 5/11/22 so it shouldn't incur any cost. https://cloud.google.com/translate/pricing
-Click the hambuger again, and hover over APIS and Services, and click Identifiers.
-Near the top of the screen, there is a button called Create Credentials. Click it.
-Create an API Key, and copy it into this script as the API_KEY variable
-Also need to install  polib 
-"""
-
-
 def get_arguments():
     if not API_KEY:
         raise ValueError("Please generate a Google API key and paste it in this file. It's needed to authenticate")
-    parser = argparse.ArgumentParser(description="Calls Google Translate API to get translations")
-    parser.add_argument("-f", "--file", type=str, help="File to translate. Accepts plain text docs or .po files")
-    parser.add_argument("-s", "--source", type=str, help="language to translate from.")
-    parser.add_argument("-t", "--target", type=str, nargs="?", default="en", help="language to translate to. default=en")
-    return parser.parse_args()
+    else:
+        parser = argparse.ArgumentParser(description="Calls Google Translate API to get translations")
+        parser.add_argument("-f", "--file", type=str, help="File to translate. Accepts plain text docs or .po files")
+        parser.add_argument("-s", "--source", type=str, help="language to translate from.")
+        parser.add_argument("-t", "--target", type=str, nargs="?", default="en", help="language to translate to. default=en")
+        return parser.parse_args()
 
 def translate_text(source, target, text):
-    """Sends request to Google Translate API and returns translated text"""
     data = {"q": text, "source": source, "target": target}
     response = requests.post(f"https://translation.googleapis.com/language/translate/v2?key={API_KEY}", data=data).json()
     if "error" in response:
         raise ValueError(f"Error status with status code: {response['error']['code']} and message: {response['error']['message']}")
-    return response["data"]["translations"][0]["translatedText"]
+    else:
+        return response["data"]["translations"][0]["translatedText"]
 
 def start_translation(args):
     """Opens file, translates all text and writes output file"""
@@ -60,7 +44,7 @@ def start_translation(args):
         pofile = polib.pofile(file)
         for entry in pofile:
             if entry.msgid and not entry.msgstr:
-                entry.msgstr = translate_text(entry.msgid)
+                entry.msgstr = translate_text(source, target, entry.msgid)
         pofile.save(f"{path}/{file_name}_{target}.po")
 
 if __name__ == "__main__":
